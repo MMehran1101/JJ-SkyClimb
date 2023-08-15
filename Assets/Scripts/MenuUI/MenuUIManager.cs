7b914f73b1cab68321698b8f0da61e74faca23bf
Mehran Fallah <m.mehran.fallah@gmail.com>
2023-08-15 22:42:05 +0430

    Complate Play and Mute Sound Meun UI

---
+++ Assets/Scripts/MenuUI/MenuUIManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Image = UnityEngine.UI.Image;
+
+namespace MenuUI
+{
+    public class MenuUIManager : MonoBehaviour
+    {
+        private bool _isSoundMute;
+        [Header("Sound")]
+        [SerializeField] private Image soundSprite; 
+        [SerializeField] private Sprite unmuteSound; 
+        [SerializeField] private Sprite muteSound; 
+        
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void MuteSound()
+        {
+            if (_isSoundMute)
+            {
+                soundSprite.sprite = unmuteSound;
+                _isSoundMute = false;
+            }
+            else
+            {
+                soundSprite.sprite = muteSound;
+                _isSoundMute = true;
+            }
+        }
+    }
+}
